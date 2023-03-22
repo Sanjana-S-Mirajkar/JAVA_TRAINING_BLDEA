61ebd544abc4029c3233b1dde8941b4d4aec615c
sanja <sanja@LAPTOP-56FKA066>
2023-03-22 21:53:53 +0530

    Pattern program

---
+++ src/main/java/TrainingCodes/LUIclass.java
@@ -0,0 +1,140 @@
+package TrainingCodes;
+
+import java.util.Scanner;
+
+public class LUIclass {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of n:");
+		int n = sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for (int j=0;j<n;j++)
+			{                                                     //I
+				if ( i==0 && j<n/2 || j==n/4 || i==n-1 && j<n/2  )
+				{
+				System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.print("  ");
+			for (int j=0;j<n;j++)
+			{                                                   //L
+				if ( j==0 || i==n-1 && j<=n/2)
+				{
+				System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for (int j=0;j<n;j++)
+			{                                                     //O
+				if (j==0 || j==n/2 || i==0 && j<=n/2 || i==n-1 && j<=n/2 )
+				{
+				System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for (int j=0;j<n;j++)
+			{                                                         //V
+				if ( j==0 && i<=n/2 || i-j==n/2 || i+j==(n-1)+(n/2) || j==n-1 && i<=n/2)
+				{
+				System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for (int j=0;j<n;j++)
+			{                                                      //E
+				if ( j==0 || i==0 && j<=n/2 || i==n/2 && j<=n/2 || i==n-1 && j<=n/2)
+				{
+				System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for (int j=0;j<n;j++)
+			{                                                      //M
+				if ( j==0 || j==n-1 || (i==j && i<=n/2 ) || (i+j==n-1 && j>=n/2))
+				{
+				System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for (int j=0;j<n;j++)
+			{                                                        //Y
+				if ( i==j && i<=n/2 || i+j==n-1 && i<=n/2 || j==n/2 && i>=n/2)
+				{
+				System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for (int j=0;j<n;j++)
+			{                                                         //I
+				if (i==0 && j<n/2 || j==n/4 || i==n-1 && j<n/2 )
+				{
+				System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for (int j=0;j<n;j++)
+			{                                                //N
+				if ( j==0 || i==j || j==n-1 )
+				{
+				System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for (int j=0;j<n;j++)
+			{                                                    //D
+				if ( j==0 || i==0 && j<n/2 || j==n/2 && i!=0 && i!=n-1 || i==n-1 && j<n/2)
+				{
+				System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for (int j=0;j<n;j++)
+			{                                                    //I
+				if (i==0 && j<n/2 || j==n/4 || i==n-1 && j<n/2)
+				{
+				System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for (int j=0;j<n;j++)
+			{                                                    //A
+				if (j==0 || j==n/2 || i==0 && j<=n/2 || i==n/2 && j<=n/2)
+				{
+				System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			
+        System.out.println();
+	}
+
+}
+}
+
+
